77dbf80d54f08e70a64948f93585733b344cae4e
root <root@chefvm1.centos.com>
2017-03-16 14:59:44 +0530

    zip file

---
+++ zipcook/README.md
@@ -0,0 +1,4 @@
+# zipcook
+
+TODO: Enter the cookbook description here.
+
---
+++ zipcook/metadata.rb
@@ -0,0 +1,19 @@
+name 'zipcook'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures zipcook'
+long_description 'Installs/Configures zipcook'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/zipcook/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/zipcook' if respond_to?(:source_url)
